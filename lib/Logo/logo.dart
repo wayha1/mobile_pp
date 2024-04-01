8ae16eb494fbc86f6afd07c5c3a4be792f80c477
JackyDom <sopheaoudom8@gmail.com>
2024-04-01 14:01:43 +0700

    update logo screen

--- lib/Logo/logo.dart
+++ lib/Logo/logo.dart
@@ -10,31 +10,34 @@ class logo extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
       duration: 5000,
-      splash: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Lottie animation
-          Lottie.asset(
-            "assets/Animation - 1711807417209.json",
-            width: 300,
-            height: 300,
-          ),
-          // Welcome text
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: Text(
-              'Welcome',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      splash: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Lottie animation
+            Lottie.asset(
+              "assets/Animation - 1711805700503.json",
+              width: 310,
+              height: 310,
+            ),
+            // Welcome text
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                'Welcome',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       nextScreen: const SignIn(),
-      splashIconSize: 200,
+      splashIconSize: 400,
       backgroundColor: Colors.green.shade200,
     );
   }
 }
+
